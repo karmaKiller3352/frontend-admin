bac6ad3da0e2a6ff229e91220097c9f634ac8e21
Suleiman <suleiman.aliev@jetruby.com>
2020-09-10 12:18:24 +0300

    before_ejecting

--- src/components/CustomEditor/index.jsx
+++ src/components/CustomEditor/index.jsx
@@ -1,36 +1,62 @@
 import React, { useEffect } from 'react';
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CKEditor from 'ckeditor4-react';
 
 function CustomEditor() {
 	useEffect(() => {
+		console.log(process.env);
 		document.title = `Arthows admin`;
 	});
 
 	return (
 		<React.Fragment>
 			<CKEditor
+				data='<p>New article</p>'
 				config={{
-					ckfinder: {
-						// Upload the images to the server using the CKFinder QuickUpload command.
-						uploadUrl: '/image-upload',
-					},
-				}}
-				editor={ClassicEditor}
-				data='<p>Hello from CKEditor 5!</p>'
-				onInit={(editor) => {
-					// You can store the "editor" and use when it is needed.
-					console.log('Editor is ready to use!', editor);
-				}}
-				onChange={(event, editor) => {
-					const data = editor.getData();
-					console.log({ event, editor, data });
-				}}
-				onBlur={(event, editor) => {
-					console.log('Blur.', editor);
-				}}
-				onFocus={(event, editor) => {
-					console.log('Focus.', editor);
+					width: '100%',
+					height: '500',
+					filebrowserImageUploadUrl:
+						'http://localhost:2000/image-upload',
+					toolbarGroups: [
+						{
+							name: 'paragraph',
+							groups: [
+								'list',
+								'indent',
+								'blocks',
+								'align',
+								'bidi',
+								'paragraph',
+							],
+						},
+						{
+							name: 'Source',
+						},
+						{
+							name: 'basicstyles',
+							groups: ['basicstyles'],
+						},
+						{
+							name: 'links',
+							groups: ['links'],
+						},
+
+						{
+							name: 'document',
+							groups: ['mode'],
+						},
+						{
+							name: 'insert',
+							groups: ['insert'],
+						},
+						{
+							name: 'styles',
+							groups: ['styles'],
+						},
+						{
+							name: 'about',
+							groups: ['about'],
+						},
+					],
 				}}
 			/>
 		</React.Fragment>
